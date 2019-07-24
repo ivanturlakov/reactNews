71410386b1ca1b9bad0a29e1ce59eb39678e8e77
Ivan <turlakov84@gmail.com>
2019-07-24 15:50:15 +0300

    firestore, add links, fetch links, display links

--- src/components/Auth/validateCreateLink.js
+++ src/components/Auth/validateCreateLink.js
@@ -1,1 +1,18 @@
-export default function validateCreateLink(values) {}
+export default function validateCreateLink(values) {
+    let errors = {}
+
+    // Email errors
+    if(!values.description) {
+        errors.description = "Description required"
+    } else if(values.description.length < 10) {
+        errors.description = "Description must be at least 10 characters"
+    }
+    // Password errors
+    if(!values.url) {
+        errors.url = "URL required"
+    } else if(!/^(ftp|http|https):\/\/[^ "]+$/.test(values.url)) {
+        errors.url = "Not valid URL"
+    }
+
+    return errors    
+}
